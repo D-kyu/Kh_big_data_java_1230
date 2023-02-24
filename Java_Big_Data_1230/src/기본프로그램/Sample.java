626884bf0605235e7be6c6778b3350c64cf792ee
D-Kyu <dkrara93@naver.com>
2023-02-24 11:45:29 +0900

    test

--- Java_Big_Data_1230/src/기본프로그램/Sample.java
+++ Java_Big_Data_1230/src/기본프로그램/Sample.java
@@ -4,6 +4,7 @@ package 기본프로그램; // 접근제한자(캡슐화)와 연관성이 있음
 public class Sample { // 클래스 이름을 지정,
 //main 메소스는 프로그램의 시작 위치를 의미하며 JVM이 호출 함.
     public static void main(String[] args) {
+
         System.out.println("Hello, Java");
     }
 }
